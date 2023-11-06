d7f3a68b2f59b14d36a4caa658cadf7e42d2fe6b
8-bitgeek <zengqiangzhao@outlook.com>
2023-11-06 13:17:47 +0000

    adjust telescope config file comment

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 local builtin = require("telescope.builtin")
 
 -- 进入 telescope 页面会是插入模式，回到正常模式就可以用 j/k 来移动了
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) 	-- 检索文件中的内容, 环境里要安装 ripgrep
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})               -- 查找文件
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {}) 	            -- 检索文件中的内容, 环境里要安装 ripgrep
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})                  -- 查找缓冲区中的文件
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})                -- 查找帮助文件
