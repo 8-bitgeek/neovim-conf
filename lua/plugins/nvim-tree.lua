05564856a35441d362934ed7c19bd86bb254c23c
8-bitgeek <zengqiangzhao@outlook.com>
2023-11-07 03:18:55 +0000

    add vim-tree config for typing "?" to show help document

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,3 +5,12 @@ vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup()
 
 vim.keymap.set('n', '<leader>v', ":NvimTreeFindFile<CR>")
+
+local function opts(desc)
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+end
+
+local api = require("nvim-tree.api")
+
+-- vim.keymap.set('n', 's', api.node.open.vertical, opts("Open: Vertical Split"))
+vim.keymap.set('n', '?', api.tree.toggle_help, opts("Help"))
