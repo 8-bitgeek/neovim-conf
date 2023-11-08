d0174d9adcde8f72fec91b05935f4e93c338b43a
8-bitgeek <zengqiangzhao@outlook.com>
2023-11-08 07:02:55 +0000

    add some keymaps and close mode message of noice

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -16,8 +16,13 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- ----------------- 普通模式下的配置 -----------------
 keymap.set("n", "<leader>nh", ":noh<CR>")                   -- 关闭搜索高亮
 keymap.set("n", "<leader>g", ":NvimTreeToggle<CR>")	        -- 打开/关闭 nvim-tree
-keymap.set("n", "<leader>ss", ":vs<CR>")                    -- 水平分屏
-keymap.set("n", "<leader>sp", ":sp<CR>")                    -- 垂直分屏
+keymap.set("n", "<leader>sv", ":vs<CR>")                    -- 水平分屏
+keymap.set("n", "<leader>sh", ":sp<CR>")                    -- 垂直分屏
+keymap.set("n", "<leader>sc", "<C-w>c")                     -- 关闭当前窗口
+keymap.set("n", "<leader>so", "<C-w>o")                     -- 关闭其它窗口
+keymap.set("n", "<leader>t", ":sp | terminal<CR>")          -- 下方打开 terminal
+keymap.set("n", "<leader>vt", ":vsp | terminal<CR>")        -- 右侧打开 terminal
+keymap.set("n", "<leader>bc", ":tabclose<CR>")              -- 关闭当前 buffer
 -- 窗口跳转 --
 keymap.set("n", "<C-h>", "<C-w>h")
 keymap.set("n", "<C-j>", "<C-w>j")
